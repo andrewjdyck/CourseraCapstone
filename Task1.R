6f8ab67b173736a97951a7d708a982b86b2d587b
Andrew Dyck <dycka@fcc.ca>
2016-11-15 16:20:58 -0600

    initial commit

---
+++ Task1.R
@@ -0,0 +1,7 @@
+
+
+datadir <- 'C:/Users/dycka/Downloads/Coursera-SwiftKey/final/en_US/'
+filenames <- c('en_US.blogs.txt', 'en_US.news.txt', 'en_US.twitter.txt')
+
+dataset:con <- file("en_US.twitter.txt", "r") readLines(con, 1)
+
