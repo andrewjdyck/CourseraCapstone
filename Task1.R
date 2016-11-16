7066d1a9d30ee3355e12d7db18d1d61c00fa20b3
Andrew Dyck <dycka@fcc.ca>
2016-11-16 17:48:02 -0600

    Week 1 complete

--- Task1.R
+++ Task1.R
@@ -3,5 +3,30 @@
 datadir <- 'C:/Users/dycka/Downloads/Coursera-SwiftKey/final/en_US/'
 filenames <- c('en_US.blogs.txt', 'en_US.news.txt', 'en_US.twitter.txt')
 
-dataset:con <- file("en_US.twitter.txt", "r") readLines(con, 1)
+
+conT <- file(paste0(datadir, filenames[3]), "r")
+conB <- file(paste0(datadir, filenames[1]), "r")
+conN <- file(paste0(datadir, filenames[2]), "r")
+# readLines(conT, 10)
+dT <- readLines(conT)
+dB <- readLines(conB)
+dN <- readLines(conN)
+
+# Love/Hate
+length(grep('love', dT))/length(grep('hate', dT))
+
+# Biostats tweet
+dT[grep('biostats', dT)]
+
+# Number of exact tweets
+length(grep('A computer once beat me at chess, but it was no match for me at kickboxing', dt))
+
+
+
+max(sapply(dB, FUN=nchar)) # max length = 40835
+max(sapply(dN, FUN=nchar)) # max length = 5760
+
+# Quiz 1
+# en_US.blog.txt is 200 MB
+
 
