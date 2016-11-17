05bb69edae30ab7588de8306860534908f6f8646
Andrew Dyck <dycka@fcc.ca>
2016-11-17 09:02:55 -0600

    Rename Task1.R to Week1.R

--- Week1.R
+++ Week1.R
@@ -1,17 +1,21 @@
 
-
+# Location of the stored data
 datadir <- 'C:/Users/dycka/Downloads/Coursera-SwiftKey/final/en_US/'
-filenames <- c('en_US.blogs.txt', 'en_US.news.txt', 'en_US.twitter.txt')
 
+# Dataset connections
+conT <- file(paste0(datadir, 'en_US.twitter.txt'), "r")
+conB <- file(paste0(datadir, 'en_US.blogs.txt'), "r")
+conN <- file(paste0(datadir, 'en_US.news.txt'), "r")
 
-conT <- file(paste0(datadir, filenames[3]), "r")
-conB <- file(paste0(datadir, filenames[1]), "r")
-conN <- file(paste0(datadir, filenames[2]), "r")
-# readLines(conT, 10)
+# Inspect the first line of a dataset
+# readLines(conT, 1)
+
+# load all the datasets into memory. Each is about 200MB. Careful!
 dT <- readLines(conT)
 dB <- readLines(conB)
 dN <- readLines(conN)
 
+# Quiz questions
 # Love/Hate
 length(grep('love', dT))/length(grep('hate', dT))
 
@@ -21,12 +25,11 @@ dT[grep('biostats', dT)]
 # Number of exact tweets
 length(grep('A computer once beat me at chess, but it was no match for me at kickboxing', dt))
 
-
-
+# Max length of row in dataset.
 max(sapply(dB, FUN=nchar)) # max length = 40835
 max(sapply(dN, FUN=nchar)) # max length = 5760
 
-# Quiz 1
+# Size of file
 # en_US.blog.txt is 200 MB
 
 
